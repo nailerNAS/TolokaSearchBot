69f4efefc02a26fd527c6b6876425a76c92d34f7
nailer <nailer@pm.me>
2018-11-14 12:30:22 +0200

    delete old service file and shortcuts add requirements.txt

--- start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-systemctl status toloka
-sudo systemctl enable toloka
-sudo systemctl restart toloka
-systemctl status toloka
--- stop.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-systemctl status toloka
-sudo systemctl disable toloka
-sudo systemctl stop toloka
-systemctl status toloka
