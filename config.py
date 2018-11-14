f1ab4b19fb8d2e77fb9a9f10aa496d16cc297008
nailer <nailer@pm.me>
2018-11-14 12:25:53 +0200

    add webhook url

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ TOKEN = environ.get('TOKEN')
 WEBHOOK_HOST = environ.get('HOST')
 WEBHOOK_PORT = 443
 WEBHOOK_URL_PATH = f'/{TOKEN}/'
+WEBHOOK_URL = f'https://{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_URL_PATH}'
 
 WEBAPP_PORT = environ.get('PORT')
